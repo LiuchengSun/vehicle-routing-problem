14843f7fa7ac2fb748a849eedd0991aa735e4546
钟行 <liucheng.slc@alibaba-inc.com>
2019-10-12 23:45:04 +0800

    基本框架

--- src/TJ332/Main.java
+++ src/TJ332/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        // ≤‚ ‘operator
+        // ≤‚ ‘
         List<Integer> route = new ArrayList<>();
         for (int i = 0; i < 11 ; i++) route.add(i);
         route.add(0);
